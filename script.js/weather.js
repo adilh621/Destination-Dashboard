74b476f671698199b4a855f1c8e6ee4298359ec1
Victoria Lee <victoriacmlee@gmail.com>
2020-09-23 14:43:54 -0400

    updated assets with screenshots

--- script.js/weather.js
+++ script.js/weather.js
@@ -124,94 +124,94 @@ $(departBtn).on('click', function (e) {
 
 // PETER'S code for appending info and icon on the dashboard and for displaying drawings
 
-// $(departBtn).on('click', function (e) {
-//     e.preventDefault();
+$(departBtn).on('click', function (e) {
+    e.preventDefault();
 
-//     // get value
-//     var cityDash = $('#cityInput').val();
-//     dashboardIcons(cityDash);
+    // get value
+    var cityDash = $('#cityInput').val();
+    dashboardIcons(cityDash);
 
-// });
+});
 
-// function dashboardIcons(cityDash) {
-//     var APIkey = "af6923e95cbb6c53be8ceb07c2b776e5";
-//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityDash + "&units=imperial&appid=" + APIkey;
+function dashboardIcons(cityDash) {
+    var APIkey = "af6923e95cbb6c53be8ceb07c2b776e5";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityDash + "&units=imperial&appid=" + APIkey;
 
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
-//         showImages(response);
-//         var latP = response.coord.lat;
-//         var lonP = response.coord.lon;
-//         // ADD THIS
-//         var temp = response.main.temp;
-//         var country = response.sys.country;
-//         // var icon = $("<img>").empty().attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
-//         $("#weather-input").empty().append(temp.toFixed(0) + " °F");
-//         $("#currency-input").empty().attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
-//         $("#language-input").empty().append(country);
-//         getDate(latP, lonP);
-//     });
-// }
+        showImages(response);
+        var latP = response.coord.lat;
+        var lonP = response.coord.lon;
+        // ADD THIS
+        var temp = response.main.temp;
+        var country = response.sys.country;
+        // var icon = $("<img>").empty().attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+        $("#weather-input").empty().append(temp.toFixed(0) + " °F");
+        $("#currency-input").empty().attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+        $("#language-input").empty().append(country);
+        getDate(latP, lonP);
+    });
+}
 
-// function getDate(latP, lonP) {
-//     var APIkey = "af6923e95cbb6c53be8ceb07c2b776e5";
-//     var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + latP + "&lon=" + lonP;
+function getDate(latP, lonP) {
+    var APIkey = "af6923e95cbb6c53be8ceb07c2b776e5";
+    var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + latP + "&lon=" + lonP;
 
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function(response) {
-//         $("#time-input").empty().append("(" + response.date_iso.split("T")[0] + ")");
-//     });
-// }
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        $("#time-input").empty().append("(" + response.date_iso.split("T")[0] + ")");
+    });
+}
 
-// function showImages(response) {
+function showImages(response) {
 
-//     var group235 = "./assets/img_dash_group235.jpg"
-//     var group6 = "./assets/img_dash_group6.jpg"
-//     var group7 = "./assets/img_dash_group7.jpg"
-//     var group8 ="./assets/img_dash_group8.jpg"
-//     var groupElse = "./assets/img_dash_group_else.jpg"
+    var group235 = "./assets/img_dash_group235.png"
+    var group6 = "./assets/img_dash_group6.png"
+    var group7 = "./assets/img_dash_group7.png"
+    var group8 ="./assets/img_dash_group8.png"
+    var groupElse = "./assets/img_dash_group_else.png"
     
-//     var ide = response.weather[0].id;
+    var ide = response.weather[0].id;
 
-//     console.log(response.weather[0].id)
-//     console.log(ide);
+    console.log(response.weather[0].id)
+    console.log(ide);
     
-//     // dashboard container
-//     var toBringContainer = $("#to-bring-container")
+    // dashboard container
+    var toBringContainer = $("#to-bring-container")
     
-//     // if for ID 200-300-500s
-//     if (parseInt(ide) < 600) {
-//         toBringContainer.attr("src", group235)
-//         console.log("rain");
-//     }
+    // if for ID 200-300-500s
+    if (parseInt(ide) < 600) {
+        toBringContainer.attr("src", group235)
+        console.log("rain");
+    }
     
-//     // if for ID 600s
-//     else if (parseInt(ide) > 599 && parseInt(ide) < 624) {
-//         toBringContainer.attr("src", group6)
-//         console.log("group 6 snow")
+    // if for ID 600s
+    else if (parseInt(ide) > 599 && parseInt(ide) < 624) {
+        toBringContainer.attr("src", group6)
+        console.log("group 6 snow")
     
-//     }
+    }
     
-//     // if for ID 700s
-//     else if (parseInt(ide) > 700 && parseInt(ide) < 800) {
-//         toBringContainer.attr("src", group7)
-//         console.log("group 7 atmosphere")
-//     }
+    // if for ID 700s
+    else if (parseInt(ide) > 700 && parseInt(ide) < 800) {
+        toBringContainer.attr("src", group7)
+        console.log("group 7 atmosphere")
+    }
     
-//     // if for ID 800s
-//     else if (parseInt(ide) === 800) {
-//         toBringContainer.attr("src", group8)
-//         console.log("group 8 clear")
-//     }
+    // if for ID 800s
+    else if (parseInt(ide) === 800) {
+        toBringContainer.attr("src", group8)
+        console.log("group 8 clear")
+    }
     
-//     // if for ID anything else just in case
-//     else {
-//         toBringContainer.attr("src", groupElse)
-//         console.log("other")
-//     };
-// }
+    // if for ID anything else just in case
+    else {
+        toBringContainer.attr("src", groupElse)
+        console.log("other")
+    };
+}
