ceff81be443ccb90ce6b50b468a2713eecc2326f
Piotr Dudek <piotr72@gmail.com>
2020-09-23 00:47:50 -0400

    updated icons' names in dashboard.html, added code to append information and drawings to dashboard - line 125 and below

--- script.js/weather.js
+++ script.js/weather.js
@@ -113,3 +113,105 @@ $(departBtn).on('click', function (e) {
 //     $toBringContainer.attr("src",toBringImages.groupElse)
 //     console.log("other")
 // };
+
+
+
+
+
+
+
+
+
+// PETER'S code for appending info and icon on the dashboard and for displaying drawings
+
+// $(departBtn).on('click', function (e) {
+//     e.preventDefault();
+
+//     // get value
+//     var cityDash = $('#cityInput').val();
+//     dashboardIcons(cityDash);
+
+// });
+
+// function dashboardIcons(cityDash) {
+//     var APIkey = "af6923e95cbb6c53be8ceb07c2b776e5";
+//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityDash + "&units=imperial&appid=" + APIkey;
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+
+//         showImages(response);
+//         var latP = response.coord.lat;
+//         var lonP = response.coord.lon;
+//         // ADD THIS
+//         var temp = response.main.temp;
+//         var country = response.sys.country;
+//         // var icon = $("<img>").empty().attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+//         $("#weather-input").empty().append(temp.toFixed(0) + " °F");
+//         $("#currency-input").empty().attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+//         $("#language-input").empty().append(country);
+//         getDate(latP, lonP);
+//     });
+// }
+
+// function getDate(latP, lonP) {
+//     var APIkey = "af6923e95cbb6c53be8ceb07c2b776e5";
+//     var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + latP + "&lon=" + lonP;
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         $("#time-input").empty().append("(" + response.date_iso.split("T")[0] + ")");
+//     });
+// }
+
+// function showImages(response) {
+
+//     var group235 = "./assets/img_dash_group235.jpg"
+//     var group6 = "./assets/img_dash_group6.jpg"
+//     var group7 = "./assets/img_dash_group7.jpg"
+//     var group8 ="./assets/img_dash_group8.jpg"
+//     var groupElse = "./assets/img_dash_group_else.jpg"
+    
+//     var ide = response.weather[0].id;
+
+//     console.log(response.weather[0].id)
+//     console.log(ide);
+    
+//     // dashboard container
+//     var toBringContainer = $("#to-bring-container")
+    
+//     // if for ID 200-300-500s
+//     if (parseInt(ide) < 600) {
+//         toBringContainer.attr("src", group235)
+//         console.log("rain");
+//     }
+    
+//     // if for ID 600s
+//     else if (parseInt(ide) > 599 && parseInt(ide) < 624) {
+//         toBringContainer.attr("src", group6)
+//         console.log("group 6 snow")
+    
+//     }
+    
+//     // if for ID 700s
+//     else if (parseInt(ide) > 700 && parseInt(ide) < 800) {
+//         toBringContainer.attr("src", group7)
+//         console.log("group 7 atmosphere")
+//     }
+    
+//     // if for ID 800s
+//     else if (parseInt(ide) === 800) {
+//         toBringContainer.attr("src", group8)
+//         console.log("group 8 clear")
+//     }
+    
+//     // if for ID anything else just in case
+//     else {
+//         toBringContainer.attr("src", groupElse)
+//         console.log("other")
+//     };
+// }
